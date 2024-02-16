30ae34b3f8885056fb918b95f264b7d685a881ab
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2024-02-17 00:55:15 +0300

    Тест хука линтера в пре-коммите

---
+++ ex.py
@@ -0,0 +1,11 @@
+from typing import (
+    Self,
+)
+
+
+class Pet:
+    def __init__(self: Self):
+        return
+
+
+x: str = 'ffffffsssssssssssssdsssssssqqqqqwqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqwqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq'
