999aa7e5a865c35da44b29ae9a5593de740feefd
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2024-02-25 22:02:52 +0300

    Update studio_developer.py

--- game/models/studio_developer.py
+++ game/models/studio_developer.py
@@ -11,8 +11,16 @@ class StudioDeveloper(models.Model):
         help_text="Название студии",
         verbose_name="Студия",
     )
-    site = models.URLField(blank=True, help_text="Сайт разработчика", verbose_name="Сайт")
-    description = models.TextField(blank=True, help_text="Подробное описание студии", verbose_name="Описание")
+    site = models.URLField(
+        blank=True,
+        help_text="Сайт разработчика",
+        verbose_name="Сайт"
+    )
+    description = models.TextField(
+        blank=True,
+        help_text="Подробное описание студии",
+        verbose_name="Описание"
+    )
 
     class Meta:
         """Метаинформация для данной модели."""
